eb7e2440c443215d126edf2df733d9265ca68c0c
Bryan Veloso <bryan@velo.so>
2026-01-26 10:00:00 -0800

    Add ACNH profile app skeleton.

---
+++ apps/profiles/acnh/apps.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from django.apps import AppConfig
+
+
+class ACNHConfig(AppConfig):
+    name = "apps.profiles.acnh"
+    verbose_name = "Animal Crossing: New Horizons"
